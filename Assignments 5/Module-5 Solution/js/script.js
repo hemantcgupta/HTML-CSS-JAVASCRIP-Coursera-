96d518293e192fb0e93649bf7d481adf681f8584
hemant gupta <guptavijay951@gmail.com>
2022-07-10 13:11:16 +0530

    Assignment 4 Done

--- Assignments 5/Module-5 Solution/js/script.js
+++ Assignments 5/Module-5 Solution/js/script.js
@@ -1,5 +1,5 @@
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Hemant", "Jignesh", "julee", "jeams", "vijay", "shruti", "Hitesh", "Aniket", "Shubham", "Jagu"];
 
 for (var i = 0; i < names.length; i++) 
 {
